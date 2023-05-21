10ef1a961d9d03ced5a7d8f436359830737abc27
Nan0MK <nan0mk@outlook.com>
2023-05-21 00:50:05 -0600

    Initial function pointers stuff

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Nan0MK-Learning-Lib
- A library of stuff that I'm learning or need to learn*
+ A library(or something) of stuff that I'm learning or need to learn*
---
+++ function_pointer/fplearn.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int addYay(int numIn1, int numIn2){
+    int out = numIn1 + numIn2;
+    return out;
+}
+int subYay(int numIn1, int numIn2){
+    int out = numIn1 - numIn2;
+    return out;
+}
+
+// *math is the function pointer. The pointer it's self takes in 2 intagers.
+// Whatever function *math points to also has to be an intager function and has to take in 2 intagers.
+int (*math)(int, int);
+
+int main(){
+
+    //math = addYay; This would point *math to addYay(). Any integers passed into *math will now be passed into addYay().
+    int oper;
+    std::cin >> oper;
+    if(oper == 0){
+        math = addYay;
+    }
+    if(oper == 1){
+        math = subYay;
+    }
+
+    int a,b;
+    std::cin >> a;
+    std::cin >> b;
+    int answer = math(a, b);
+
+    std::cout << answer << std::endl;
+
+    return 0;
+}
